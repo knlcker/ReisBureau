034b296ad1ecd7543f7b2e1f9b97adaae79348da
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2023-05-17 12:31:44 +0200

    Log in

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 <body>
     <header class="header-main">
