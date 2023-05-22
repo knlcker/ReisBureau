e34eb164376a7df5e325eb6c871f6e7f44c578f9
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2023-05-22 12:44:35 +0200

    registreren

--- Inloggen.php
+++ Inloggen.php
@@ -25,7 +25,7 @@
             <div class="inloggen-information-container">
                 <a href="index.php"><i class="fa-solid fa-arrow-left" style="color: #000000;"></i>Terug</a>
                 <div class="inloggen-registreren-container">
-                    <div class="inloggen-container">
+                    <div id="inloggen" class="inloggen-container">
                         <img class="bookly-image-inloggen" src="Images/Bookly.png" alt="Bookly-image">
                         <form class="form-inloggen" account="" method="POST">
                             <div class="email-wachtwoord-input">
@@ -38,7 +38,25 @@
                         <div class="naar-registreren-container">
                             <p>Heb je nog geen account?</p>
                             <div class="naar-registratie">
-                                <p>Maak er dan </p> <div class="inloggen-registratie-switch">hier</div> <p> een aan!</p>
+                                <p>Maak er dan</p> <div class="inloggen-registratie-switch" onclick="switchNaarRegistreren()">hier</div> <p>een aan!</p>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    <div id="registreren" class="inloggen-container">
+                        <img class="bookly-image-inloggen" src="Images/Bookly.png" alt="Bookly-image">
+                        <form class="form-inloggen" account="" method="POST">
+                            <div class="email-wachtwoord-input">
+                                <input class="inloggen-input" type="text" name="email-login" placeholder="E-Mail..."></input>
+                                <input class="inloggen-input" type="text" name="password-login" placeholder="Wachtwoord..."></input>
+                            </div>
+                            <submit class="inloggen-submit">Maak account!</submit>
+                        </form>
+                        <div class="naar-registreren-container">
+                            <p>Heb je al een account?</p>
+                            <div class="naar-registratie">
+                                <p>Klik dan</p> <div class="inloggen-registratie-switch" onclick="switchNaarInloggen()">hier</div> <p>om in te loggen!</p>
                             </div>
                             
                         </div>
@@ -48,5 +66,18 @@
             </div>
         </div>
     </main>
+
+    <script>
+        function switchNaarRegistreren(){
+            document.getElementById("inloggen").style.display = "none";
+            document.getElementById("registreren").style.display = "flex";
+
+        };
+
+        function switchNaarInloggen(){
+            document.getElementById("inloggen").style.display = "flex";
+            document.getElementById("registreren").style.display = "none";
+        };
+    </script>
 </body>
 </html>
