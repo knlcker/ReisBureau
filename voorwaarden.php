94e27201b0355b67c59f669ade91e143b40d9aad
nick <117974842+knlcker@users.noreply.github.com>
2023-06-05 12:37:43 +0200

    algemenen voorwaarden + cookie policy + privacy policy (#34)

---
+++ voorwaarden.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>voorwaarden</title>
+    <link rel="stylesheet" href="/styles/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    
+</head>
+<p><body>
+    <div class="voorwaarden">
+<h1>Algemene voorwaarden</h1>
+<h2>Welkom bij bookly!</h2>
+
+<p>Deze algemene voorwaarden omschrijven de regels en voorschriften voor het gebruik van de website van bookly, gelegen op bookly.com.</p>
+
+<p>Door deze website te bezoeken, gaan we ervan uit dat je deze algemene voorwaarden accepteert. Blijf bookly niet gebruiken als je niet akkoord gaat met alle voorwaarden die op deze pagina worden vermeld.</p>
+</div>
+<div class="voorwaarden">
+<h3>Cookies:</h3>
+<p>De website maakt gebruik van cookies om je online ervaring persoonlijker te maken. Door bookly te openen, stemde je ermee in om de vereiste cookies te gebruiken.</p>
+<p>
+<p>Een cookie is een tekstbestand dat door een web pagina server op je harde schijf wordt geplaatst. Cookies kunnen niet worden gebruikt om programma's uit te voeren of virussen op je computer te plaatsen. Cookies worden uniek aan jou toegewezen en kunnen alleen worden gelezen door een webserver in het domein dat de cookie aan je heeft uitgegeven.</p>
+<p>
+<p>We kunnen cookies gebruiken om informatie te verzamelen, op te slaan en bij te houden voor statistische of marketingdoeleinden om onze website te laten werken. Je hebt de mogelijkheid om optionele cookies te accepteren of om te weigeren. Er zijn enkele vereiste cookies die nodig zijn voor de werking van onze website. Voor deze cookies is je toestemming niet vereist, aangezien ze altijd werken. Hou er rekening mee dat door het accepteren van vereiste cookies, je ook cookies van derden accepteert, die kunnen worden gebruikt via door derden geleverde services als je dergelijke services op onze website gebruikt, bijvoorbeeld een video weergavevenster van derden dat geïntegreerd is op onze website.</p>
+</div>
+<div class="voorwaarden">
+<h3>Licentie:</h3>
+<p>Tenzij anders vermeld, bezit bookly en/of zijn licentiegevers de intellectuele eigendomsrechten voor alle gegevens op bookly. Alle intellectuele eigendomsrechten zijn voorbehouden. Je hebt hier toegang toe vanaf bookly voor je eigen persoonlijk gebruik, onderworpen aan de beperkingen die in deze algemene voorwaarden zijn vastgelegd.</p>
+</div>
+<div class="voorwaarden">
+<h3>Je mag geen:</h3>
+<p>
+<p>Materiaal van bookly kopiëren of opnieuw publiceren</p>
+<p>Materiaal van bookly verkopen, verhuren of in sublicentie geven</p>
+<p>materiaal van bookly reproduceren, dupliceren of kopieren</p>
+<p>Herdistribueren van inhoud van bookly</p>
+<p>Deze overeenkomst begint op deze datum.</p>
+
+<p>Delen van deze website bieden gebruikers de mogelijkheid om meningen en informatie op bepaalde delen van de website te plaatsen en uit te wisselen. bookly filtert, bewerkt, publiceert of beoordeelt geen opmerkingen vóór hun aanwezigheid op de website. Reacties weerspiegelen geen standpunten en meningen van bookly, zijn agenten en/of affiliate ondernemingen. Opmerkingen weerspiegelen de standpunten en meningen van de persoon die zijn opvattingen en meningen plaatsen. Voor zover toegestaan ​​door de toepasselijke wetgeving, is bookly niet aansprakelijk voor de opmerkingen of enige aansprakelijkheid, schade of kosten veroorzaakt en/of geleden als gevolg van het gebruik van, en/of het plaatsen van, en/of de verschijning van de reacties op deze website.</p>
+
+<p>bookly behoudt zich het recht voor om alle opmerkingen te controleren en alle opmerkingen te verwijderen die als ongepast of beledigend kunnen worden beschouwd of die inbreuk maken op deze algemene voorwaarden.</p>
+</div>
+<div class="voorwaarden">
+<h3>Je garandeert en verklaart dat:</h3>
+<p>
+<p>Je het recht hebt om de opmerkingen op onze website te plaatsen en beschikt over alle benodigde licenties en toestemmingen om dit te doen;</p>
+<p>De opmerkingen maken geen inbreuk op enig intellectueel eigendomsrecht, inclusief maar niet beperkt tot auteursrechten, patenten of handelsmerken van een derde partij;</p>
+<p>De opmerkingen bevatten geen lasterlijk, lasterlijk, beledigend, onfatsoenlijk of anderszins onwettig materiaal, wat een inbreuk op de privacy is.</p>
+<p>De opmerkingen zullen niet worden gebruikt om zakelijke of aangepaste of huidige commerciële activiteiten of onwettige activiteiten te vragen of te promoten.</p>
+<p>Hierbij verleen je bookly een niet-exclusieve licentie voor het gebruiken, reproduceren, bewerken en autoriseren van anderen om je opmerkingen te gebruiken, reproduceren en bewerken in alle vormen, formaten, of media.</p>
+</div>
+<div class="voorwaarden">
+<h3>Hyperlinks naar onze inhoud:</h3>
+<h3>De volgende organisaties kunnen naar onze website linken zonder voorafgaande schriftelijke toestemming:</h3>
+
+<p>Overheidsinstanties;</p>
+<p>Zoekmachines;</p>
+<p>Nieuwsorganisaties;</p>
+<p>Online directory-distributeurs kunnen op dezelfde manier naar onze website linken als hyperlinks naar de websites van andere beursgenoteerde bedrijven; en</p>
+<p>Geaccrediteerde bedrijven voor het hele systeem, met uitzondering van het werven van non-profitorganisaties, winkelcentra voor goede doelen en fondsenwerving groepen voor liefdadigheidsinstellingen die geen hyperlink naar onze website mogen hebben.</p>
+<p>Deze organisaties kunnen linken naar onze startpagina, publicaties of andere website informatie, zolang de link: (a) op geen enkele manier misleidend is; (b) impliceert niet ten onrechte sponsoring, goedkeuring of goedkeuring van de verbindende partij en haar producten en/of diensten; en (c) past binnen de context van de site van de verbindende partij.</p>
+</div>
+<div class="voorwaarden">
+<h3>We kunnen andere linkverzoeken van de volgende soorten organisaties in overweging nemen en goedkeuren:</h3>
+
+<p>algemeen bekende informatiebronnen voor consumenten en/of bedrijven;</p>
+<p>dot.com-gemeenschap sites;</p>
+<p>verenigingen of andere groepen die goede doelen vertegenwoordigen;</p>
+<p>online directory-distributeurs;</p>
+<p>internetportalen;</p>
+<p>accountants-, advocaten- en adviesbureaus; en</p>
+<p>onderwijsinstellingen en handelsverenigingen.</p>
+<p>We zullen linkverzoeken van deze organisaties goedkeuren als we besluiten dat: (a) de link ons ​​er niet gunstig uit zou laten zien voor onszelf of voor onze geaccrediteerde bedrijven; (b) de organisatie geen negatieve gegevens bij ons heeft; (c) het voordeel voor ons van de zichtbaarheid van de hyperlink compenseert de afwezigheid van bookly; en (d) de link staat in de context van algemene broninformatie.</p>
+
+<p>Deze organisaties kunnen naar onze homepage linken zolang de link: (a) op geen enkele manier misleidend is; (b) geen ten onrechte sponsoring impliceert, goedkeuring van de verbindende partij en haar producten of diensten heeft; en (c) past binnen de context van de site van de verbindende partij.</p>
+
+<p>Als je een van de organisaties bent die worden genoemd in paragraaf 2 hierboven, en geïnteresseerd bent in een link naar onze website, moet je ons hiervan op de hoogte stellen door een e-mail te sturen naar bookly. Vermeld je naam, de naam van je organisatie, contactgegevens evenals de URL van je site, een lijst met alle URL's van waaruit je naar onze website wilt linken en een lijst met de URL's op onze site waarnaar je wilt verwijzen. koppeling. Wacht 2-3 weken op een reactie.</p>
+</div>
+<div class="voorwaarden">
+<h3>Goedgekeurde organisaties kunnen als volgt een hyperlink naar onze website maken:</h3>
+
+<p>Door gebruik te maken van onze bedrijfsnaam; of</p>
+<p>Door gebruik te maken van de uniforme resource locator waarnaar wordt gelinkt; of</p>
+<p>Het gebruik van een andere omschrijving van onze website waarnaar gelinkt wordt, het moet logisch zijn binnen de context en het formaat van de inhoud op de site van de koppelende partij.</p>
+<p>Het gebruik van het logo van bookly of ander artwork is niet toegestaan ​​voor het linken zonder een handelsmerk licentieovereenkomst.</p>
+</div>
+<div class="voorwaarden">
+<h3>Aansprakelijkheid voor inhoud:</h3>
+<p>Wij kunnen niet verantwoordelijk worden gehouden voor enige inhoud die op je website verschijnt. Je stemt ermee in om ons te beschermen en te verdedigen tegen alle claims die op je website worden ingediend. Er mogen geen link(s) op een website verschijnen die kunnen worden geïnterpreteerd als lasterlijk, obsceen of crimineel, of die inbreuk maken, anderszins schendt of pleit voor de inbreuk of andere schending van rechten van derden.</p>
+</div>
+<div class="voorwaarden">
+<h3>Voorbehoud van rechten:</h3>
+<p>We behouden ons het recht voor om te verzoeken dat je alle links of een bepaalde link naar onze website verwijdert. Je stemt ermee in om op verzoek alle links naar onze website onmiddellijk te verwijderen. We behouden ons ook het recht voor om deze algemene voorwaarden en het koppelingsbeleid op elk moment te wijzigen. Door continu naar onze website te linken, ga je ermee akkoord gebonden te zijn aan, en zich te houden aan deze voorwaarden voor linken.</p>
+
+<p>Verwijdering van links van onze website:</p>
+<p>Als je een link op onze website vindt die om welke reden dan ook aanstootgevend is, kun je op elk moment contact met ons opnemen en ons hiervan op de hoogte stellen. We zullen verzoeken om links te verwijderen in overweging nemen, maar we zijn niet verplicht om rechtstreeks op je te reageren.</p>
+
+<p>We zorgen er niet voor dat de informatie op deze website correct is. We garanderen geen volledigheid of nauwkeurigheid, noch beloven we ervoor te zorgen dat de website beschikbaar blijft of dat het materiaal op de website up-to-date wordt gehouden.</p>
+</div>
+<div class="voorwaarden">
+<h3>Disclaimer:</h3>
+<p>Voor zover maximaal toegestaan ​​door de toepasselijke wetgeving, sluiten we alle verklaringen, garanties en voorwaarden met betrekking tot onze website en het gebruik van deze website uit. Niets in deze disclaimer zal:</p>
+
+<p>onze of jouw aansprakelijkheid voor overlijden of persoonlijk letsel beperken of uitsluiten;</p>
+<p>beperking of uitsluiting van onze of jouw aansprakelijkheid voor fraude of frauduleuze verkeerde voorstelling van zaken;</p>
+<p>een van onze of jouw aansprakelijkheden beperken op een manier die niet is toegestaan ​​onder de toepasselijke wetgeving; of</p>
+<p>sluit een van onze of jouw aansprakelijkheden uit die mogelijk niet worden uitgesloten onder de toepasselijke wetgeving.</p>
+<p>De beperkingen en verbodsbepalingen van aansprakelijkheid uiteengezet in deze Sectie en elders in deze disclaimer: (a) zijn onderworpen aan de voorgaande paragraaf; en (b) alle aansprakelijkheden regelen die voortvloeien uit de disclaimer, inclusief aansprakelijkheden die voortvloeien uit een contract, onrechtmatige daad en voor schending van wettelijke plichten.</p>
+
+<p>Zolang de website en de informatie en diensten op de website gratis worden aangeboden, zijn wij niet aansprakelijk voor verlies of schade van welke aard dan ook.</p>
+</div>
+</body>
+</html>
