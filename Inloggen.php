f2c86f1ec164ff17624f301b719a3d6c9dd93b68
Niels van den Broek <123738989+NielsVDBroek@users.noreply.github.com>
2023-06-07 13:38:05 +0200

    Merge dit (#40)

--- Inloggen.php
+++ Inloggen.php
@@ -68,11 +68,18 @@
                                 </div>
                                 <div class="form-registratie-compact">
                                     <select class="registratie-land" type="" name="registreren-land" placeholder="Land" required>
-                                        <option value="Nederland">Nederland</option>
-                                        <option value="Duitsland">Duitsland</option>
-                                        <option value="Engeland">Engeland</option>
-                                        <option value="Spanje">Spanje</option>
-                                        <option value="Amerika">Amerika</option>
+                                        <?php
+                                            $statement = $connectie->prepare("SELECT * FROM landen ORDER BY land_name ASC");
+                                            $statement->execute([]);
+
+                                            if ($statement->rowCount() == 0) {
+                                                echo "Er zijn geen zoekresultaten gevonden!";
+                                            } else {
+                                                while ($item = $statement->fetch()) {
+                                                    echo'<option value="' . $item['land_id'] . '">' . $item['land_name'] . '</option>';
+                                                }
+                                            }
+                                        ?>
                                     </select>
                                     <input class="registratie-geboortedatum" type="date" name="registreren-geboortedatum" placeholder="Geboortedatum" required></input>
                                 </div>
