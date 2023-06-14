0ac292b7bb73da9395fd7217aa49a4138bf71563
Niels van den Broek <123738989+NielsVDBroek@users.noreply.github.com>
2023-06-14 12:17:15 +0200

    searching complete (#49)

--- homepage-reiszoeken.php
+++ homepage-reiszoeken.php
@@ -1,5 +1,9 @@
 <?php 
     session_start();
+
+    function isPresent($waarde) {
+        return isset($waarde) && $waarde != "";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,98 +27,127 @@
         } catch (PDOException $e) {
             echo "Verbinding werkt niet" . $e;
         }
-        $date = date('Y-m-d');
+        $date = date('Y-m-d+1');
     ?>
 </head>
 <body>
     <header class="header-main">
-        <img class="bookly-image" src="Images/Bookly.png" alt="Bookly-image">
+        <a href="index.php"><img class="bookly-image" src="Images/Bookly.png" alt="Bookly-image"></a>        
         <?php 
             if(isset($_SESSION['user_firstname'])){
                 echo '<a href="account.php">' . $_SESSION['user_firstname'] . '</a>';
-            } else echo '<a href="Inloggen.php">Inloggen</a>';
+            } else {
+                echo '<a href="Inloggen.php">Inloggen</a>';
+            }
         ?>
     </header>
     <main class="main-index">
         <img class="index-image" src="Images/index3.png" alt="Twee stoelen op het strand">
             <div class="reis-search-container">
                 <form class="reis-search-container" action="homepage-reiszoeken.php" method="GET">
-                    <input class="reis-search" name="search_location" type="search" placeholder="Locatie..." ></input>
-                    <input class="reis-search" name="search_date_start" type="date" min="<?php echo $date;?>" placeholder="Aankomst"></input>
-                    <input class="reis-search" name="search_date_end" type="date" min="<?php echo $date;?>" placeholder="Vertrek"></input>
+                    <input class="reis-search" name="search_location" type="search" placeholder="Locatie..." <?php if(isset($_GET['search_location'])){echo 'value="' . $_GET['search_location'] . '"';} ?> ></input>
+                    <input class="reis-search" name="search_date_start" type="date" min="<?php echo $date;?>" placeholder="Aankomst" <?php if(isset($_GET['search_date_start'])){echo 'value="' . $_GET['search_date_start'] . '"';} ?>></input>
+                    <input class="reis-search" name="search_date_end" type="date" min="<?php echo $date;?>" placeholder="Vertrek" <?php if(isset($_GET['search_date_end'])){echo 'value="' . $_GET['search_date_end'] . '"';} ?>></input>
                 </form>
             </div>
 
         <div class="reis-locaties-container">
             <?php
+            if(count($_GET) == 0) {
+                $resultSet = $connectie->prepare("SELECT * FROM reizen");
+                $resultSet->execute([]);
+            } else {
 
-            if(isset($_GET['search_location']) && !isset($_GET['search_date_start']) && !isset($_GET['search_date_end'])){
+            if(isset($_GET['search_location']) && isPresent($_GET['search_location']) && isPresent($_GET['search_date_start']) && isPresent($_GET['search_date_end'])){
+                $search_input = $_GET['search_location'];
+                $search_date_start = $_GET['search_date_start'];
+                $search_date_end = $_GET['search_date_end'];
+                if($search_date_start >= $search_date_end){
+                    echo "Vertrek datum kan niet eerder dan of gelijk zijn als aankomst, Pas uw zoekopdracht aan!";
+                } else {
+                    $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE concat(reis_title, reis_description, reis_location_country, reis_location_city) LIKE ? AND reis_available_start <= ? AND reis_available_end >= ?");
+                    $resultSet->execute(["%" . $search_input . "%", $search_date_start, $search_date_end]); 
+                }
+                
+            }
+            else if(isPresent($_GET['search_location']) && !isPresent($_GET['search_date_start']) && !isPresent($_GET['search_date_end'])){
                 $search_input = $_GET['search_location'];
                 $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE concat(reis_title, reis_description, reis_location_country, reis_location_city) LIKE ?");
                 $resultSet->execute(["%" . $search_input . "%"]);
 
-            } else if(!isset($_GET['search_location']) && isset($_GET['search_date_start']) && !isset($_GET['search_date_end'])){
+            } 
+            
+            // Geen locatie en enddate, wel start date
+            else if(!isPresent($_GET['search_location']) && isPresent($_GET['search_date_start']) && !isPresent($_GET['search_date_end'])){
                 $search_date_start = $_GET['search_date_start'];
                 $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE reis_available_start <= ?");
                 $resultSet->execute([$search_date_start]);
 
-            } else if(!isset($_GET['search_location']) && !isset($_GET['search_date_start']) && isset($_GET['search_date_end'])){
+            } else if(!isPresent($_GET['search_location']) && !isPresent($_GET['search_date_start']) && isPresent($_GET['search_date_end'])){
                 $search_date_end = $_GET['search_date_end'];
                 $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE reis_available_end >= ?");
                 $resultSet->execute([$search_date_end]);
 
 
-            } else if(isset($_GET['search_location']) && isset($_GET['search_date_start']) && !isset($_GET['search_date_end'])){
+            } else if(isPresent($_GET['search_location']) && isPresent($_GET['search_date_start']) && !isPresent($_GET['search_date_end'])){
                 $search_input = $_GET['search_location'];
                 $search_date_start = $_GET['search_date_start'];
-                $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE concat(reis_title, reis_description, reis_location_country, reis_location_city) LIKE ? AND WHERE reis_available_start <= ?");
+                $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE concat(reis_title, reis_description, reis_location_country, reis_location_city) LIKE ? AND reis_available_start <= ?");
                 $resultSet->execute(["%" . $search_input . "%", $search_date_start]);
 
-            } else if(isset($_GET['search_location']) && !isset($_GET['search_date_start']) && isset($_GET['search_date_end'])){
+            } else if(isPresent($_GET['search_location']) && !isPresent($_GET['search_date_start']) && isPresent($_GET['search_date_end'])){
                 $search_input = $_GET['search_location'];
                 $search_date_end = $_GET['search_date_end'];
-                $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE concat(reis_title, reis_description, reis_location_country, reis_location_city) LIKE ? AND WHERE reis_available_end >= ?");
+                $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE concat(reis_title, reis_description, reis_location_country, reis_location_city) LIKE ? AND reis_available_end >= ?");
                 $resultSet->execute(["%" . $search_input . "%", $search_date_end]);
 
-            } else if(isset($_GET['search_location']) && !isset($_GET['search_date_start']) && !isset($_GET['search_date_end'])){
+            } else if(!isPresent($_GET['search_location']) && isPresent($_GET['search_date_start']) && isPresent($_GET['search_date_end'])){
                 $search_date_start = $_GET['search_date_start'];
                 $search_date_end = $_GET['search_date_end'];
-                $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE reis_available_start <= ? AND WHERE reis_available_end >= ?");
-                $resultSet->execute([$search_date_start, $search_date_end]);
+                if($search_date_start >= $search_date_end){
+                    echo "Vertrek datum kan niet eerder dan of gelijk zijn als aankomst, Pas uw zoekopdracht aan!";
+                } else {
+                    $resultSet = $connectie->prepare("SELECT * FROM reizen WHERE reis_available_start <= ? AND reis_available_end >= ?");
+                    $resultSet->execute([$search_date_start, $search_date_end]);   
+                }
+                
 
             } else{
                 $resultSet = $connectie->prepare("SELECT * FROM reizen");
                 $resultSet->execute([]);
             }
+        }
 
-            if($resultSet->rowCount() == 0){
-                echo "Er zijn geen reizen gevonden met deze zoekresultaten!";
-            } else{
-                while ($item = $resultSet->fetch()) {
-                    echo '
-                    <div class="reis-container">
-                        <div class="left-container-reis-homepage">
-                            <div class="reis-img">foto</div>
-                        </div>
+            if(isset($resultSet)){
+                if($resultSet->rowCount() == 0){
+                    echo "Er zijn geen reizen gevonden met deze zoekresultaten!";
+                } else{
+                    while ($item = $resultSet->fetch()) {
+                        echo '
+                        <div class="reis-container">
+                            <div class="left-container-reis-homepage">
+                                <div class="reis-img">foto</div>
+                            </div>
                     
-                        <div class="middle-container-reis-homepage">
-                            <div class="middle">
-                                <div class="location">' . $item['reis_location_country'] . " , " . $item['reis_location_city'].'</div>
-                                <div class="accommodation">' . $item['reis_title'] .'</div>
-                                <div class="divider"></div>
-                                <div class="reviews-container">
-                                    <div class="reviews">' . $item['reis_gemiddelde_review'] .'/10</div>
-                                    <div class="reviews">' . $item['reis_aantal_reviews'] .'(beoordelingen)</div>
+                            <div class="middle-container-reis-homepage">
+                                <div class="middle">
+                                    <div class="location">' . $item['reis_location_country'] . " , " . $item['reis_location_city'].'</div>
+                                    <div class="accommodation">' . $item['reis_title'] .'</div>
+                                    <div class="divider"></div>
+                                    <div class="reviews-container">
+                                        <div class="reviews">' . $item['reis_gemiddelde_review'] .'/10</div>
+                                        <div class="reviews">' . $item['reis_aantal_reviews'] .'(beoordelingen)</div>
+                                    </div>
+                                </div>
+                                <div class="right-container-reis-homepage">
+                                    <div class="price">€ ' . $item['reis_price'] .'</div>
                                 </div>
                             </div>
-                            <div class="right-container-reis-homepage">
-                                <div class="price">€ ' . $item['reis_price'] .'</div>
-                            </div>
-                        </div>
     
-                    </div>
-                            ';
+                        </div>
+                        ';
                 }
+            }
             }
             ?>
         </div>
