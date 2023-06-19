609eb13f598bb97b6bb2842d866df4f743c5e5df
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2023-06-19 14:42:07 +0200

    Reis kan boeken
    reis boekingen moeten nog verbeterd worden

--- reis-boekingen.php
+++ reis-boekingen.php
@@ -13,10 +13,36 @@ session_start();
 
     $date = date('Y-m-d');
 
-if(isset($_POST['Locatie-land']) && isset($_POST['Locatie-stad']) && isset($_POST['Beschrijving']) && isset($_POST['Prijs']) && isset($_POST['Start-datum']) && isset($_POST['Eind-datum']) && isset($_POST['Beschrijving-reis']) && isset($_POST['hoofd-afbeelding'])){
-    
+    if(isset($_POST['reis_id_to_book'])) {
+        $reis_ophalen = $connectie->prepare("SELECT * FROM reizen where reis_id = ?");
+        $reis_ophalen->execute([$_POST['reis_id_to_book']]);
 
-}
+        $reis = $reis_ophalen->fetch();
+    }
 
-header("Location: account.php")
-?>
+    if(isset($_POST['reis_id_to_book']) && isset($_POST['aantal-persoonen']) && isset($_POST['datum-aankomst']) && isset($_POST['datum-vertrek'])){
+        if($_POST['datum-aankomst'] >= $_POST['datum-vertrek']){
+            header("Location: homepage-reiszoeken.php");
+        }
+
+        $reis_id = $_POST['reis_id_to_book'];
+        $user_id = $_SESSION['user_id'];
+        $reis_aantal_personen = $_POST['aantal-persoonen'];
+        $reis_datum_aankomst = $_POST['datum-aankomst'];
+        $reis_datum_vertrek = $_POST['datum-vertrek'];
+        $reis_prijs_per_nacht = $reis['reis_price'];
+
+        $datum1=date_create($reis_datum_aankomst);
+        $datum2=date_create($reis_datum_vertrek);
+        $verschil=date_diff($datum1,$datum2);
+        $aantal_dagen=$verschil->format("%a");
+
+        $prijs= ($aantal_dagen * $reis_prijs_per_nacht * $reis_aantal_personen);
+
+        $statement = $connectie->prepare("INSERT INTO boekingen(reis_id, user_id, boeking_reis_start, boeking_reis_end, boeking_aantal_personen, boeking_price) VALUES(?, ?, ?, ?, ?, ?)");
+        $statement->execute([$reis_id, $user_id, $reis_datum_aankomst, $reis_datum_vertrek, $reis_aantal_personen, $prijs]);
+
+    }
+
+    header("Location: account.php")
+    ?>
