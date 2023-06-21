7113a16ddc5664d090e770a09bd4f7132fe5dcb8
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2023-06-22 00:07:21 +0200

    recensie voltoois

--- recensie-verwerken.php
+++ recensie-verwerken.php
@@ -14,19 +14,19 @@ session_start();
     $date = date('Y-m-d');
     $date_plus_one_day = date('Y-m-d', strtotime("+1 day"));
 
-if(isset($_POST['reis_review'])){
-    $reisReview = $_POST['reis_review'];
-    $reisReviewBeoordeling = $_POST['reis-review-beoordeling'];
-    if(isset($_POST['recensie-bericht'])){
-        $reisReviewBericht = $_POST['recensie-bericht']; 
-
-        $addReview = $connectie->prepare("INSERT INTO reviews(reis_id, reis_review_beoordeling, reis_review_bericht) VALUES(?, ?, ?)");
-        $addReview->execute([$reisReview, $reisReviewBeoordeling, $reisReviewBericht]);
-    } else{
-        $addReview = $connectie->prepare("INSERT INTO reviews(reis_id, reis_review_beoordeling) VALUES(?, ?)");
-        $addReview->execute([$reisReview, $reisReviewBeoordeling]);
+    if(isset($_POST['add_geboekte_reis_to_review'])){
+        $reisReviewID = $_POST['add_geboekte_reis_to_review'];
+        $reisReviewBeoordeling = $_POST['reis_review_beoordeling'];
+        if(isset($_POST['recensie_bericht'])){
+            $reisReviewBericht = $_POST['recensie_bericht']; 
+    
+            $addReview = $connectie->prepare("UPDATE boekingen SET reis_review_beoordeling = ?, reis_review_bericht = ? WHERE boeking_id = ?");
+            $addReview->execute([$reisReviewBeoordeling, $reisReviewBericht, $reisReviewID]);
+        } else{
+            $addReview = $connectie->prepare("UPDATE boekingen SET reis_review_beoordeling = ? WHERE boeking_id = ?");
+            $addReview->execute([$reisReviewBeoordeling, $reisReviewID]);
+        }
     }
-}
 
 header("Location: account.php")
 ?>
