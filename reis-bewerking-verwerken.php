8e24de082e30e06bf14a3f7dc32e049daa6de51e
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2023-06-22 01:05:19 +0200

    Reis Bewerken Done

--- reis-bewerking-verwerken.php
+++ reis-bewerking-verwerken.php
@@ -13,6 +13,7 @@ session_start();
 
     $date = date('Y-m-d');
     $date_plus_one_day = date('Y-m-d', strtotime("+1 day"));
+    $ID_reis_to_edit = $_POST['ID_reis_to_edit'];
 
 if(isset($_POST['Locatie-land']) && isset($_POST['Locatie-stad']) && isset($_POST['Beschrijving']) && isset($_POST['Prijs']) && isset($_POST['Start-datum']) && isset($_POST['Eind-datum']) && isset($_POST['Beschrijving-reis']) && isset($_POST['hoofd-afbeelding'])){
     $new_locatie_land = $_POST['Locatie-land'];
@@ -25,8 +26,8 @@ if(isset($_POST['Locatie-land']) && isset($_POST['Locatie-stad']) && isset($_POS
     $new_locatie_beschrijving_reis = $_POST['Beschrijving-reis'];
     $new_locatie_hoofd_afbeelding = $_POST['hoofd-afbeelding'];
 
-    $reis_toevoegen = $connectie->prepare("INSERT into reizen(reis_title, reis_description, reis_location_country, reis_location_city, reis_price, reis_max_people, reis_available_start, reis_available_end, reis_main_photo, reis_status) VALUES(?, ?, ?, ?, ?, ?, ?, ?, 'OPEN')");
-    $reis_toevoegen->execute([$new_locatie_overnachting_beschrijving, $new_locatie_beschrijving_reis, $new_locatie_land, $new_locatie_stad, $new_locatie_prijs, $new_max_aantal_personen, $new_locatie_start_datum, $new_locatie_eind_datum, $new_locatie_hoofd_afbeelding]);
+    $reis_bewerken = $connectie->prepare("UPDATE reizen SET reis_location_country = ?, reis_location_city = ?, reis_title = ?, reis_price = ?, reis_max_people = ?, reis_available_start = ?, reis_available_end = ?, reis_description = ? WHERE reis_id = ?");
+    $reis_bewerken->execute([$new_locatie_land, $new_locatie_stad, $new_locatie_overnachting_beschrijving, $new_locatie_prijs, $new_max_aantal_personen, $new_locatie_start_datum, $new_locatie_eind_datum, $new_locatie_beschrijving_reis, $ID_reis_to_edit]);
 }
 
 
