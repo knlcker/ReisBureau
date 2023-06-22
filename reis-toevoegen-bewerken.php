272800469453d6f19a43d78d6e58d5ba78d5a5d5
Niels van den Broek <123738989+NielsVDBroek@users.noreply.github.com>
2023-06-22 11:48:37 +0200

    FINALLL!!! (#79)
    Reis toevoegen miste value. nu niet meer

--- reis-toevoegen-bewerken.php
+++ reis-toevoegen-bewerken.php
@@ -14,7 +14,7 @@ session_start();
     $date = date('Y-m-d');
     $date_plus_one_day = date('Y-m-d', strtotime("+1 day"));
 
-if(isset($_POST['Locatie-land']) && isset($_POST['Locatie-stad']) && isset($_POST['Beschrijving']) && isset($_POST['Prijs']) && isset($_POST['Start-datum']) && isset($_POST['Eind-datum']) && isset($_POST['Beschrijving-reis']) && isset($_POST['hoofd-afbeelding'])){
+if(isset($_POST['Locatie-land']) && isset($_POST['Locatie-stad']) && isset($_POST['Beschrijving']) && isset($_POST['Prijs']) && isset($_POST['Start-datum']) && isset($_POST['Eind-datum']) && isset($_POST['Beschrijving-reis'])){
     $new_locatie_land = $_POST['Locatie-land'];
     $new_locatie_stad = $_POST['Locatie-stad'];
     $new_locatie_overnachting_beschrijving = $_POST['Beschrijving'];
@@ -23,12 +23,16 @@ if(isset($_POST['Locatie-land']) && isset($_POST['Locatie-stad']) && isset($_POS
     $new_locatie_start_datum = $_POST['Start-datum'];
     $new_locatie_eind_datum = $_POST['Eind-datum'];
     $new_locatie_beschrijving_reis = $_POST['Beschrijving-reis'];
-    $new_locatie_hoofd_afbeelding = $_POST['hoofd-afbeelding'];
+    if(isset($_POST['hoofd-afbeelding'])){
+        $new_locatie_hoofd_afbeelding = $_POST['hoofd-afbeelding'];
+        $reis_toevoegen = $connectie->prepare("INSERT into reizen(reis_title, reis_description, reis_location_country, reis_location_city, reis_price, reis_max_people, reis_available_start, reis_available_end, reis_main_photo, reis_status) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, 'OPEN')");
+        $reis_toevoegen->execute([$new_locatie_overnachting_beschrijving, $new_locatie_beschrijving_reis, $new_locatie_land, $new_locatie_stad, $new_locatie_prijs, $new_max_aantal_personen, $new_locatie_start_datum, $new_locatie_eind_datum, $new_locatie_hoofd_afbeelding, ]);
+    } else{
+        $reis_toevoegen = $connectie->prepare("INSERT into reizen(reis_title, reis_description, reis_location_country, reis_location_city, reis_price, reis_max_people, reis_available_start, reis_available_end, reis_status) VALUES(?, ?, ?, ?, ?, ?, ?, ?, 'OPEN')");
+        $reis_toevoegen->execute([$new_locatie_overnachting_beschrijving, $new_locatie_beschrijving_reis, $new_locatie_land, $new_locatie_stad, $new_locatie_prijs, $new_max_aantal_personen, $new_locatie_start_datum, $new_locatie_eind_datum]);
 
-    $reis_toevoegen = $connectie->prepare("INSERT into reizen(reis_title, reis_description, reis_location_country, reis_location_city, reis_price, reis_max_people, reis_available_start, reis_available_end, reis_main_photo, reis_status) VALUES(?, ?, ?, ?, ?, ?, ?, ?, 'OPEN')");
-    $reis_toevoegen->execute([$new_locatie_overnachting_beschrijving, $new_locatie_beschrijving_reis, $new_locatie_land, $new_locatie_stad, $new_locatie_prijs, $new_max_aantal_personen, $new_locatie_start_datum, $new_locatie_eind_datum, $new_locatie_hoofd_afbeelding]);
+    }
 }
-
 
 
 header("Location: account.php")
